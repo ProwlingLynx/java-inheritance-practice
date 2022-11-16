cd861b5f838fc2d5366a853a272658cc8f6da0f0
ProwlingLynx <tamezjavier@gmail.com>
2022-11-15 18:32:04 -0600

    Adding bike class Adding bike logic in main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
+import Bike.Bike;
 import cars.FordFusion;
 
 public class Main {
     public static void main(String[] args) {
         FordFusion myCar = new FordFusion(3, 60, "black", 4, 10, 0);
-
+        Bike myBike = new Bike(30, 15);
 
         myCar.refuel(3);
         myCar.drive();
@@ -13,6 +14,12 @@ public class Main {
         myCar.drive();
         myCar.getWheelCount();
         myCar.crashAndBurn();
+
+        System.out.println("\n---------------------------------\n");
+
+        myBike.fastPedal();
+        myBike.getWheelCount();
+        myBike.getPedalCount();
 
 
 
